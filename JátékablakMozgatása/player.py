0604e441d5aaa19eb8d797bce388698b054aa863
sim05 <siadgyu@gmail.com>
2024-02-14 19:03:34 +0100

    fix

--- JátékablakMozgatása/player.py
+++ JátékablakMozgatása/player.py
@@ -4,7 +4,7 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.image.load("./img/player/Idle__000.png").convert_alpha()
+        self.image = pygame.image.load("../img/player/Idle__000.png").convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 8
